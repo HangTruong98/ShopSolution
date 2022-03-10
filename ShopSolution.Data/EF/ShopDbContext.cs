91aacdcedc1013b0d94198073eddd5e3868a8986
Hang Truong <hangtruong11398@gmail.com>
2022-03-10 21:45:42 +0700

    Add Entity

---
+++ ShopSolution.Data/EF/ShopDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopSolution.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopSolution.Data.EF
+{
+    public class ShopDbContext : DbContext
+    {
+        public ShopDbContext( DbContextOptions options) : base(options)
+        {
+           
+
+        }
+        public DbSet<Product> Products { set; get; }
+        public DbSet<Category> Categories { set; get; }
+
+        public DbSet<Cart> Carts { set; get; }
+        public DbSet<Order> Orders { set; get; }
+        public DbSet<OrderDetail> OrderDetails { set; get; }
+        public DbSet<Transaction> Transactions { set; get; }
+        public DbSet<ProductTranslation> ProductTranslations { set; get; }
+        public DbSet<CategoryTranslation> CategoryTranslations { set; get; }
+        public DbSet<Promotion> Promotions { set; get; }
+        public DbSet<SystemActivity> SystemActivities { set; get; }
+        public DbSet<Contact> Contacts { set; get; }
+        public DbSet<Language> Languages { set; get; }
+        public DbSet<AppConfig> AppConfigs { set; get; }
+    }
+}
---
+++ ShopSolution.Data/Entities/Language.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopSolution.Data.Entities
+{
+    public class Language
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public bool IsDefault { set; get; }
+    }
+}
